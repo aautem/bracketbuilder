e380222be1db4a747c2b75761f27ec1c5f66c346
Alex Autem <autem.alex@gmail.com>
2017-01-23 22:28:39 -0500

    Add routing to the server

---
+++ server/config/routes.js
@@ -0,0 +1,14 @@
+var bracketController = require('../brackets/bracketController.js');
+
+module.exports = function (app, express) {
+  app.get('/test', function(req, res, next) {
+    console.log('Testing...');
+    Bracket.find({})
+      .then(function(results) {
+        res.send(results);
+      });
+  });
+
+  // app.get('/api/brackets/', bracketController.allBrackets);
+  // app.post('/api/brackets/', bracketController.newBracket);
+};
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
+// dependencies
 var express = require('express');
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
+// create server
 var app = express();
 
+// connect to database
 mongoose.connect('mongodb://localhost/bracketbuilder');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -12,35 +15,32 @@ db.on('open', function() {
   console.log('Database connected!');
 });
 
-var bracketSchema = mongoose.Schema({
-  name: String
-});
+// var bracketSchema = mongoose.Schema({
+//   name: String
+// });
 
-var Bracket = mongoose.model('Bracket', bracketSchema);
+// var Bracket = mongoose.model('Bracket', bracketSchema);
 
-var nba = new Bracket({ name: 'NBA Playoffs' });
+// var nba = new Bracket({ name: 'NBA Playoffs' });
 
-nba.save(function (err, nba) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(nba);
-  }
-});
+// nba.save(function (err, nba) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(nba);
+//   }
+// });
 
+// set server port
 app.set('port', (process.env.PORT || 8000));
 
+// add middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
 
-app.get('/test', function(req, res, next) {
-  console.log('Testing...');
-  Bracket.find({})
-    .then(function(results) {
-      res.send(results);
-    });
-});
+// route handling
+require('./config/routes.js')(app, express);
 
 app.listen(app.get('port'), function() {
   console.log('Listening on port', app.get('port'));
