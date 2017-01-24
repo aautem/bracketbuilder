06f7111e2bfd58b2b7ec8a32ad8f6923d913c698
Alex Autem <autem.alex@gmail.com>
2017-01-23 22:36:39 -0500

    Create bracket model

--- server/brackets/bracketModel.js
+++ server/brackets/bracketModel.js
@@ -1,1 +1,19 @@
-// bracketModel.js
+var mongoose = require('mongoose');
+
+var bracketSchema = new mongoose.Schema({
+  name: String,
+  teams: Array,
+  size: Number
+});
+
+module.exports = mongoose.model('Bracket', bracketSchema);
+
+// var nba = new Bracket({ name: 'NBA Playoffs' });
+
+// nba.save(function (err, nba) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(nba);
+//   }
+// });
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// dependencies
 var express = require('express');
 var mongoose = require('mongoose');
 var morgan = require('morgan');
@@ -15,22 +14,6 @@ db.on('open', function() {
   console.log('Database connected!');
 });
 
-// var bracketSchema = mongoose.Schema({
-//   name: String
-// });
-
-// var Bracket = mongoose.model('Bracket', bracketSchema);
-
-// var nba = new Bracket({ name: 'NBA Playoffs' });
-
-// nba.save(function (err, nba) {
-//   if (err) {
-//     console.error(err);
-//   } else {
-//     console.log(nba);
-//   }
-// });
-
 // set server port
 app.set('port', (process.env.PORT || 8000));
 
@@ -42,6 +25,7 @@ app.use(express.static(__dirname + '/../client'));
 // route handling
 require('./config/routes.js')(app, express);
 
+// start the server
 app.listen(app.get('port'), function() {
   console.log('Listening on port', app.get('port'));
 });
