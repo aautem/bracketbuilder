ff67b19a879e7232f3aab155d00660820a94aa80
Alex Autem <autem.alex@gmail.com>
2017-01-23 22:55:04 -0500

    Add method to services in bracket factory

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,1 +1,19 @@
-// services.js
+angular.module('bracket.services', [])
+
+.factory('Brackets', function($http) {
+
+  var getAll = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/brackets/'
+    })
+    .then(function(res) {
+      return res.data;
+    });
+  };
+
+  return {
+    getAll: getAll
+  };
+
+});
