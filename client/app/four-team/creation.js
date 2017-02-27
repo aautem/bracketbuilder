ee48de6b27390f74432886036b61e5ab813d9444
Alex Autem <autem.alex@gmail.com>
2017-02-26 20:29:38 -0500

    Update styling to bracket view

--- client/app/four-team/creation.js
+++ client/app/four-team/creation.js
@@ -7,16 +7,6 @@ angular.module('bracket.create', [])
   $scope.data.bracket.teams = [];
   $scope.data.bracket.size = 4;
 
-  $scope.addTitle = function() {
-    $scope.data.bracket.name = prompt('Name your bracket!', 'bracket name');
-    console.log('New bracket name:', $scope.data.bracket.name);
-  };
-
-  $scope.addTeam = function(index) {
-    $scope.data.bracket.teams[index] = prompt('Add a team!', 'team name');
-    console.log('Teams in bracket:', $scope.data.bracket.teams);
-  };
-
   $scope.advanceTeam = function(curIndex, nextIndex) {
     console.log('ADVANCING TEAM...');
     $scope.data.bracket.teams[nextIndex] = $scope.data.bracket.teams[curIndex];
@@ -28,13 +18,13 @@ angular.module('bracket.create', [])
 
   $scope.saveBracket = function() {
     if ($scope.data.bracket.name === '') {
-      alert('Bracket must have title!');
+      alert('Bracket must have title.');
       return;
     } else {
       console.log('Saving bracket to database...');
       // POST REQUEST TO API
       Brackets.create($scope.data.bracket);
-      alert('Bracket saved to database!');
+      alert('Bracket saved!');
     }
   };
 });
